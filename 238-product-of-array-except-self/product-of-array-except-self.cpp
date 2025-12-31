712a068f2fadbb35c69d1420bd6ad6ec3e403a11
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-01-01 01:53:36 +0530

    Time: 1 ms (49.69%) | Memory: 40.1 MB (78.91%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,18 +4,15 @@ public:
         int n = nums.size();
         vector<int> answer(n,1);
         int prefix =1;
-        for(int i=0; i<n; i++){
-            answer[i] = prefix;
-            prefix =prefix * nums[i];
+        for(int i=0; i<nums.size(); i++){
+            answer[i] =prefix;
+            prefix = prefix * nums[i];
         }
-
-        int suffix = 1;
-        for (int i = n - 1; i >= 0; i--) {
+        int suffix =1;
+        for(int i=nums.size()-1; i>=0; i--){
             answer[i] = answer[i] * suffix;
             suffix = suffix * nums[i];
         }
-
         return answer;
-        
     }
 };
