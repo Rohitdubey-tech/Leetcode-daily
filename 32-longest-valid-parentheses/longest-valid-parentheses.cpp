1f4a8a6a3cdf1b9b0ba5be6b54147e946f0e44e2
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-01-24 00:40:25 +0530

    Time: 1 ms (56.58%) | Memory: 11.9 MB (26.99%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,21 +1,23 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int>st;
-        st.push(-1);
+        int n = s.size();
+        vector<int> dp(n, 0);
         int maxLength = 0;
-        for(int i=0; i<s.size(); i++){
-            if(s[i]=='('){
-                st.push(i);
-            }
-            else{
-                st.pop();
-                if(st.empty()){
-                    st.push(i);
+
+        for (int i = 1; i < n; i++) {
+            if (s[i] == ')') {
+                if (s[i - 1] == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(') {
+                    dp[i] = dp[i - 1] + (i - dp[i - 1] >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
                 }
-                else{
-                    maxLength = max(maxLength, i-st.top());
-                }
+                maxLength = max(maxLength, dp[i]);
             }
         }
         return maxLength;
