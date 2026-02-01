5c3b6f6512334ab330ffb40ad9789ec006258989
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-02-01 23:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (48.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
-        vector<int> freq(26, 0);
-        for(int i=0; i<s.length(); i++){
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
+        vector<int> frequency(26, 0);
+        for(char c: s){
+            frequency[c - 'a']++;
         }
-        for(int i=0; i<26; i++){
-            if(freq[i] != 0){
+        for(char c: t){
+            frequency[c-'a']--;
+        }
+        for(int count: frequency){
+            if(count != 0){
                 return false;
             }
+           
         }
-        return true;
+         return true;
     }
 };
