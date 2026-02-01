b7dd743eebb93832f26432447c036035c41855ba
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-02-02 00:36:54 +0530

    Time: 2 ms (42.51%) | Memory: 11 MB (36.16%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        string word = "";
+        for(char c: s){
+            if(c==' '){
+                if(word != ""){
+                    words.push_back(word);
+                    word = "";
+                }
+            }
+                else{
+                    word += c;
+                }
+            }
+            if(word != ""){
+                words.push_back(word);
+            }
+            string result = "";
+            for(int i=words.size()-1; i>=0; i--){
+                result += words[i];
+                if(i != 0) result += " ";
+            }
+        
+        return result;
+    }
+};
