bcad8fd4f25c4c2ae035edeebb528280d7df4a2c
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-02-10 22:21:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (79.22%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.cpp
+++ 233-number-of-digit-one/number-of-digit-one.cpp
@@ -1,25 +1,37 @@
 class Solution {
-    public:
-    int countDigitOne(int n) {
-        long factor = 1;
-        int count = 0;
-
-        while (factor <= n) {
-            long higher = n / (factor * 10);
-            long current = (n / factor) % 10;
-            long lower = n % factor;
-
-            if (current == 0) {
-                count += higher * factor;
-            } else if (current == 1) {
-                count += higher * factor + lower + 1;
-            } else {
-                count += (higher + 1) * factor;
-            }
-
-            factor *= 10;
+public:
+    int power(int x, int n){
+        int power = 1;
+        for(int i=0;i<n;i++){
+            power *= x;
         }
+        return power;
+    }
 
+    int countDigitOne(int n) {
+        if(n==0) return 0;
+        int k = 0;
+        int count = 0;
+        int digit = n/(power(10,k));
+        while(!(digit<10)){
+            k++;
+            digit = n/(power(10,k));
+        }
+        while(n){
+            if (k==0){
+                count++;
+                break;
+            }
+            count += (digit*(k*power(10,k-1)));
+            if (digit > 1){
+                count +=  power(10,k);
+            }else if(digit==1){
+                count += n-power(10,k)+1;
+            }
+            n = n%(power(10,k));
+            k--;
+            digit = n/(power(10,k));
+        }
         return count;
     }
 };
