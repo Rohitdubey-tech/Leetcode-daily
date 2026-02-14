6e146979364bf9ef11e91b7ec83a0ffbcb392bd4
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-02-14 19:47:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.79%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,7 +4,7 @@ public:
         int j = 0;
         for(int i=0; i<nums.size(); i++){
             if(nums[i] != val){
-                nums[j]=nums[i];
+                nums[j] = nums[i];
                 j++;
             }
         }
