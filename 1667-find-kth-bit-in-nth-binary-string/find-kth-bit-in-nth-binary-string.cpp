c1dfec543ee3cc377f09009828d2b769dd456574
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-03-03 10:02:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (80.15%) - LeetSync

---
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        bool flip = false;
+
+        while (n > 1) {
+            int mid = 1 << (n - 1);
+            int len = (1 << n) - 1;
+
+            if (k == mid) {
+                return flip ? '0' : '1';
+            }
+
+            if (k > mid) {
+                k = len - k + 1;
+                flip = !flip;
+            }
+
+            n--;
+        }
+
+        return flip ? '1' : '0';
+    }
+};
