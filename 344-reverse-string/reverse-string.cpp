7786130ab9d4cfd712c40c1c5106b4dd81b8d430
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-03-22 11:46:11 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (51.65%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -5,7 +5,7 @@ public:
         int right = s.size()-1;
         while(left<right){
             char temp = s[left];
-            s[left]=s[right];
+            s[left]= s[right];
             s[right] = temp;
             left++;
             right--;
