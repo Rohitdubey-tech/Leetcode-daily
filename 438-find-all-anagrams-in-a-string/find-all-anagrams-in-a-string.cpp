8550a691e9a9a29cd67a1e446902916efa8a545a
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-04-17 01:27:59 +0530

    Time: 3 ms (78.14%) | Memory: 11.6 MB (36.28%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string s, string p) {
-        vector<int> result;
-        if(s.size()<p.size()) return result;
-        vector<int>pCount(26,0), sCount(26,0);
-        int pLen = p.size();
-        for(char c: p) pCount[c - 'a']++;
-        for(int i=0; i<pLen; i++) sCount[s[i] - 'a']++;
-        if(sCount == pCount) result.push_back(0);
-        for(int i=pLen; i<s.size(); i++){
-            sCount[s[i]-'a']++;
-            sCount[s[i-pLen]-'a']--;
-            if (sCount == pCount) result.push_back(i - pLen + 1);
+    bool allZero(vector<int> & counter){
+        for(int &i : counter){
+            if(i != 0){
+                return false;
+            }
         }
-        return result;
+        return true;
     }
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        int n = s.size();
+        vector<int>counter(26, 0);
+        for(int i=0; i<p.size(); i++){
+            counter[p[i]-'a']++;
+        }
+        int i=0;
+        int j=0;
+        int k = p.size();
+        while(j<n){
+            counter[s[j]-'a']--;
+            if(j-i+1 == k){
+                if(allZero(counter)){
+                    ans.push_back(i);
+                }
+                counter[s[i]-'a']++;
+                i++;
+            }
+            j++;
+        }
+        return ans;
+            }
 };
