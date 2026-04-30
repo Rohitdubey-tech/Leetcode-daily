00ac63897b8656dcb0f8a63c17eb4b0c52269bdc
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-05-01 01:03:14 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (59.83%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int minPrice = INT_MAX;
         int maxProfit = 0;
         for(int i=0; i<prices.size(); i++){
-            if(prices[i] < minPrice){
+            if(prices[i]<minPrice){
                 minPrice = prices[i];
             }
             else{
