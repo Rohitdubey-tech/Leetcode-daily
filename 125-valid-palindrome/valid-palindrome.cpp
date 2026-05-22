608dd733d49b20f8a0ee55e93e53d344fcd093ca
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-05-23 00:56:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (7.83%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string clean = "";
-        for(char c : s){
+        string temp = "";
+        for(char c: s){
             if(isalnum(c)){
-                clean += tolower(c);
+                temp += tolower(c);
             }
         }
-        string temp = clean;
-        reverse(temp.begin(), temp.end());
-        return clean == temp;
+        string rev = temp;
+        reverse(rev.begin(), rev.end());
+        return temp == rev;
     }
 };
