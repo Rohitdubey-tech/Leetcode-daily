1dcdefdfa135b7194b96d072618e8351cff2e357
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-05-23 11:31:26 +0530

    Time: 3 ms (16.46%) | Memory: 25.9 MB (76.33%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int left =0; 
-        int right = n-1;
+        int left = 0;
+        int right = height.size()-1;
         int leftMax = 0;
         int rightMax = 0;
         int water = 0;
