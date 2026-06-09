638df5c5fd1382ef8445e438949bd15d6e898962
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-06-10 02:08:25 +0530

    Time: 12 ms (9.52%) | Memory: 25.4 MB (24.01%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,20 +1,22 @@
 class Solution {
- public:
-  bool isValidSudoku(vector<vector<char>>& board) {
-    unordered_set<string> seen;
-
-    for (int i = 0; i < 9; ++i)
-      for (int j = 0; j < 9; ++j) {
-        if (board[i][j] == '.')
-          continue;
-        const string c(1, board[i][j]);
-        if (!seen.insert(c + "@row" + to_string(i)).second ||
-            !seen.insert(c + "@col" + to_string(j)).second ||
-            !seen.insert(c + "@box" + to_string(i / 3) + to_string(j / 3))
-                 .second)
-          return false;
-      }
-
-    return true;
-  }
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string>seen;
+        for(int r=0; r<9; r++){
+            for(int c=0; c<9; c++){
+                char num=board[r][c];
+                if(num == '.') continue;
+                string row = "row" + to_string(r) + num;
+                string col = "col" + to_string(c) + num;
+                string box = "box" + to_string(r/3) + to_string(c/3) + num;
+                if(seen.count(row) || seen.count(col) || seen.count(box)){
+                    return false;
+                }
+                seen.insert(row);
+                seen.insert(col);
+                seen.insert(box);
+                            }
+        }
+        return true;
+    }
 };
