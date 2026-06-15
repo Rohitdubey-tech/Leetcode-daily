6b6de3757e296a32bb3e5e7a426bc0c5bd794887
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-06-15 23:32:11 +0530

    Time: 2 ms (39.69%) | Memory: 113.8 MB (26.29%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -4,11 +4,11 @@ public:
         int n = nums.size();
         double windowSum = 0;
         for(int i=0; i<k; i++){
-            windowSum +=nums[i];
+            windowSum += nums[i];
         }
         double maxSum = windowSum;
         for(int i=k; i<n; i++){
-            windowSum += nums[i] - nums[i-k];
+            windowSum += nums[i]-nums[i-k];
             maxSum = max(maxSum, windowSum);
         }
         return maxSum/k;
