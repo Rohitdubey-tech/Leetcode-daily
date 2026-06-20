8cad69c0947e836c97040fef39df0dc2421a2eeb
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-06-21 03:24:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (60.62%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        return letters[(upper_bound(letters.begin(), letters.end(), target)-letters.begin())%letters.size()];
+        int low = 0;
+        int high = letters.size()-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(letters[mid] <= target){
+                low = mid +1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return letters[low % letters.size()];
     }
 };
