7fa3256678d4f3228f759f38dcf7972c2bac3832
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-07-05 17:36:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (33.62%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if(str1 +str2 != str2 + str1) return "";
-        int len1 = str1.size();
-        int len2 = str2.size();
-        int gcdLen = gcd(len1, len2);
-        return str1.substr(0, gcdLen);
- }
- private:
- int gcd(int a, int b){
-    return b ==0 ? a : gcd(b,a%b);
- }
+        if(str1+str2 != str2+str1) return "";
+        int len = gcd(str1.size(), str2.size());
+        return str1.substr(0, len);
 
+        
+    }
 };
