abc6b7db83161f9a70dba31fed7e8889632832dc
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-07-06 22:45:52 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (54.89%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int, int>mp;
         int n = nums.size();
         for(int i=0; i<n; i++){
-            int need = target-nums[i];
+            int need = target - nums[i];
             if(mp.find(need) != mp.end()){
                 return {mp[need], i};
             }
