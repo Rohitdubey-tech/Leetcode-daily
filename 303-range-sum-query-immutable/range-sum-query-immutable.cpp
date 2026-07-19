6d59043838f1899e4bdcd04387e3a4b83ffb59be
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-07-20 00:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (63.03%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -5,12 +5,12 @@ public:
         prefix.resize(nums.size());
         prefix[0]=nums[0];
         for(int i=1; i<nums.size(); i++){
-            prefix[i]=prefix[i-1]+nums[i];
+            prefix[i]=prefix[i-1] + nums[i];
         }
     }
     
     int sumRange(int left, int right) {
-        if(left==0){
+        if(left ==0){
             return prefix[right];
         }
         return prefix[right]-prefix[left-1];
