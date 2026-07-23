b7310cb169c4c4ae4c047ecb8e572d0b3a9752bf
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-07-23 06:19:11 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (51.26%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        int col0=1;
-        for(int i=0; i<rows; i++){
-            if(matrix[i][0]==0){
-                col0=0;
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        bool firstRow = false;
+        bool firstCol = false;
+
+        for (int j = 0; j < col; j++) {
+            if (matrix[0][j] == 0) {
+                firstRow = true;
+                break;
             }
-            for(int j=1; j<cols; j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+        }
+
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                firstCol = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i=rows-1; i>=0; i--){
-            for(int j=cols-1;j>=1; j--){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j]=0;
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
             }
-            if(col0==0){
-                matrix[i][0]=0;
+        }
+
+        if (firstRow) {
+            for (int j = 0; j < col; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (firstCol) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
