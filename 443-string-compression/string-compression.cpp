6f6d703a1361bf0a7cef29cf2db9d306527614d5
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-07-28 02:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (14.76%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         int n = chars.size();
-        int read = 0;
-        int write = 0;
-        while(read<n){
-            char curr = chars[read];
+        int i=0;
+        int index = 0;
+        while(i<n){
+            char current = chars[i];
             int count = 0;
-            while(read < n && chars[read] == curr){
+            while(i < n && chars[i]==current){
                 count++;
-                read++;
+                i++;
             }
-            chars[write] = curr;
-            write++;
-            if(count>1){
+            chars[index] = current;
+            index++;
+            if(count > 1){
                 string cnt = to_string(count);
                 for(char c : cnt){
-                    chars[write] = c;
-                    write++;
+                    chars[index] = c;
+                    index++;
                 }
             }
+
         }
-        return write;
+        return index;
     }
 };
