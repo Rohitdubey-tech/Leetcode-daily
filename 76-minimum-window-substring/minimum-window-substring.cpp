ef7eb8246f0642cb65a35b529f8cd851a9f83e61
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-08-01 04:20:30 +0530

    Time: 7 ms (61.11%) | Memory: 11.6 MB (43.43%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(t.size()>s.size()) return "";
         unordered_map<char, int>mp;
-        for(char ch : t){
-            mp[ch]++;
+        for(char c : t){
+            mp[c]++;
         }
-        int left =0;
-        int count = t.size();
-        int start =0;
-        int minLen = INT_MAX;
-        for(int right=0; right<s.size(); right++){
+        int left = 0;
+        int minLength = INT_MAX;
+        int start = 0;
+        int required = t.size();
+        for(int right = 0; right<s.size(); right++){
             if(mp[s[right]]>0){
-                count--;
+                required--;
             }
             mp[s[right]]--;
-            while(count==0){
-                if(right-left+1 < minLen){
-                    minLen = right-left+1;
+            while(required==0){
+                if(right-left+1 <minLength){
+                    minLength = right-left+1;
                     start = left;
                 }
                 mp[s[left]]++;
                 if(mp[s[left]]>0){
-                    count++;
+                    required++;
                 }
                 left++;
             }
         }
-        if(minLen == INT_MAX){
+        if(minLength == INT_MAX){
             return "";
         }
-        return s.substr(start, minLen);
+        return s.substr(start, minLength);
     }
 };
