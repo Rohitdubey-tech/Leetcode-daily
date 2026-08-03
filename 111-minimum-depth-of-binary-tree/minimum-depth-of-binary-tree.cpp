46b826fbf6919e21fad3f4db2e76f0179dd9ff68
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-08-04 05:17:08 +0530

    Time: 4 ms (44.15%) | Memory: 146.9 MB (52.94%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -12,25 +12,10 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if(root == NULL){
-            return 0;
-        }
-        queue<TreeNode*> q;
-        q.push(root);
-        int depth = 1;
-        while(!q.empty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                TreeNode* node = q.front();
-                q.pop();
-                if(node->left == NULL && node->right == NULL){
-                    return depth;
-                }
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            depth++;
-        }
-        return depth;
+        if(root == NULL) return 0;
+        if(root->left == NULL && root->right== NULL) return 1;
+        if(root->left == NULL) return 1 + minDepth(root->right);
+        if(root->right== NULL) return 1 + minDepth(root->left);
+        return 1 + min(minDepth(root->left), minDepth(root->right)); 
     }
 };
