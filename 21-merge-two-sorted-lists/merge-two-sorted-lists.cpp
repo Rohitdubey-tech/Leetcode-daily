df1a705e33a46208008270931c4a2530f44a4e81
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-08-13 03:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (28.28%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -14,18 +14,20 @@ public:
         ListNode dummy(0);
         ListNode* tail = &dummy;
         while(list1 && list2){
-            if(list1->val <list2->val){
-                tail->next = list1;
-                list1 = list1->next;
+            if(list1->val > list2->val){
+                tail->next = list2;
+                tail=tail->next;
+                list2 =  list2->next;
             }
             else{
-                tail->next = list2;
-                list2 = list2->next;
+                tail->next = list1;
+                tail = tail->next;
+                list1 = list1->next;
             }
-            tail = tail->next;
         }
             if(list1) tail->next = list1;
             else tail->next = list2;
-        return dummy.next;
+            return dummy.next;
+        
     }
 };
