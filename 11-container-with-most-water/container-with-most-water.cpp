0e6bf3126c785f7e0946aa19d015fe26bcb2edeb
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-08-21 00:47:30 +0530

    Time: 3 ms (34.46%) | Memory: 63 MB (11.12%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -3,12 +3,12 @@ public:
     int maxArea(vector<int>& height) {
         long long left = 0;
         long long right = height.size()-1;
-        long long maxWater = 0;
+        long long water = 0;
         while(left < right){
-            long long width  = right - left;
+            long long width = right - left;
             long long minHeight = min(height[left], height[right]);
             long long area = width * minHeight;
-            maxWater = max(area, maxWater);
+            water = max(water, area);
             if(height[left]<height[right]){
                 left++;
             }
@@ -16,6 +16,6 @@ public:
                 right--;
             }
         }
-        return maxWater;
+        return water;
     }
 };
