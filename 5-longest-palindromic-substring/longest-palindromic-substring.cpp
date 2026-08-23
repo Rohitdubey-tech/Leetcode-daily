9b7f63d7256888229fd9d797ca9097e3100eb5ab
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-08-23 18:30:12 +0530

    Time: 90 ms (29.24%) | Memory: 174.3 MB (10.02%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string expand(string s, int left, int right){
-        while(left>=0 && right <s.size() && s[left]==s[right]){
+        while(left>=0 && right<s.size() && s[left]==s[right]){
             left--;
             right++;
         }
@@ -10,12 +10,11 @@ public:
     string longestPalindrome(string s) {
         string ans = "";
         for(int i=0; i<s.size(); i++){
-            string s1 = expand(s, i, i);
-            string s2 = expand(s, i, i+1);
+            string s1 = expand(s,i, i);
+            string s2= expand(s, i, i+1);
             if(s1.size() > ans.size()) ans = s1;
             if(s2.size() > ans.size()) ans = s2;
         }
         return ans;
-
     }
 };
