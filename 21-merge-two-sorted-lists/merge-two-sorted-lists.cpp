6d49485debc116c359e409f1d5db5447723f0e92
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-08-23 21:02:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (63.04%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -16,18 +16,17 @@ public:
         while(list1 && list2){
             if(list1->val > list2->val){
                 tail->next = list2;
-                tail=tail->next;
-                list2 =  list2->next;
+                tail = tail->next;
+                list2 = list2->next;
             }
             else{
                 tail->next = list1;
                 tail = tail->next;
                 list1 = list1->next;
-            }
         }
-            if(list1) tail->next = list1;
-            else tail->next = list2;
-            return dummy.next;
-        
+        }
+        if(list1) tail->next = list1;
+        else tail->next = list2;
+        return dummy.next;
     }
 };
