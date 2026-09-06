28a439724a3fc552e1f063e46367e5251af23dc8
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-06 15:37:16 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (10.61%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>>ans;
         for(auto interval : intervals){
-            if(interval[1] < newInterval[0]){
+            if(interval[1]< newInterval[0]){
                 ans.push_back(interval);
             }
             else if(interval[0] > newInterval[1]){
@@ -11,12 +11,12 @@ public:
                 newInterval = interval;
             }
             else{
-                newInterval[0] = min( newInterval[0], interval[0]);
-                newInterval[1] = max( newInterval[1], interval[1]);
+                newInterval[0] = min(newInterval[0], interval[0]);
+                newInterval[1] = max(newInterval[1], interval[1]);
             }
-           
         }
         ans.push_back(newInterval);
         return ans;
+        
     }
 };
