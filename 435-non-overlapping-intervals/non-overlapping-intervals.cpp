227708fe2cffaac0e849effb01379427b9da7b2c
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-06 15:43:25 +0530

    Time: 43 ms (66.35%) | Memory: 93.9 MB (90.89%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if(intervals.size()==0) return 0;
-        sort(intervals.begin(), intervals.end(), 
-        [](const vector<int>&a, const vector<int>&b){
-            return a[1] < b[1];
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>&b){
+            return a[1]<b[1];
         });
         int count = 0;
         int prevEnd = intervals[0][1];
@@ -16,6 +15,6 @@ public:
                 prevEnd = intervals[i][1];
             }
         }
-        return count;
+        return count ;
     }
 };
