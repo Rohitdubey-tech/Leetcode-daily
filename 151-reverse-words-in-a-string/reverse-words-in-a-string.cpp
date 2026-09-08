c185c883e0a11aefdbde29e9375cb5e5c6531a9a
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-08 19:34:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (69.78%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -6,14 +6,14 @@ public:
         int n = s.size();
         string ans;
         while(i < n){
-            while(i < n && s[i]==' '){
+            while(i<n && s[i] == ' '){
                 i++;
             }
-            if(i >= n ){
+            if(i>= n){
                 break;
             }
             string word;
-            while(i<n && s[i] != ' '){
+            while(i<n && s[i] !=' '){
                 word += s[i];
                 i++;
             }
