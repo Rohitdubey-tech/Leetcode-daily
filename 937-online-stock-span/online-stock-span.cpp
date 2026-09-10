392470f7f469aaded4b48df7520f577920e64100
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-10 20:09:43 +0530

    Time: 42 ms (23.99%) | Memory: 97.2 MB (15.99%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -4,6 +4,7 @@ public:
         
     }
     stack<pair<int, int>>st;
+
     int next(int price) {
         int span = 1;
         while(!st.empty() && st.top().first <= price){
@@ -12,7 +13,6 @@ public:
         }
         st.push({price, span});
         return span;
-
     }
 };
 
