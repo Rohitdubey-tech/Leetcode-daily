f2eb07de760807a38296bbcae00f847e0bac5eeb
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-13 02:49:49 +0530

    Time: 8 ms (21.75%) | Memory: 18.5 MB (66.12%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,38 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    struct compare{
-        bool operator()(ListNode* a, ListNode* b){
-            return a->val>b->val;
+    struct compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
         }
     };
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, compare>pq;
-        for(ListNode* list : lists){
-            if(list != nullptr){
-                pq.push(list);
-            }
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+
+        for(auto head : lists) {
+            if(head != NULL)
+                pq.push(head);
         }
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
-        while(!pq.empty()){
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+
+        while(!pq.empty()) {
             ListNode* node = pq.top();
             pq.pop();
-            tail->next = node;
-            tail = tail->next;
-            if(node->next != NULL){
+
+            curr->next = node;
+            curr = curr->next;
+
+            if(node->next != NULL) {
                 pq.push(node->next);
             }
-                    }
-                    return dummy.next;
+        }
+
+        return dummy->next;
     }
 };
