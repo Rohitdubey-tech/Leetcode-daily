fd29b4e011d347dd9c0e34830b88383876c79e22
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-12 19:45:03 +0530

    Time: 31 ms (41.16%) | Memory: 42.7 MB (15.76%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -1,44 +1,38 @@
 class Codec {
 public:
-    string serialize(TreeNode* root) {
-        if (!root) return "null";
-        queue<TreeNode*> q;
-        q.push(root);
-        string res;
-        while (!q.empty()) {
-            TreeNode* node = q.front(); q.pop();
-            if (node) {
-                res += to_string(node->val) + ",";
-                q.push(node->left);
-                q.push(node->right);
-            } else {
-                res += "null,";
-            }
+    void serializeHelper(TreeNode* root, string& s) {
+        if (root == NULL) {
+            s += "#,";
+            return;
         }
-        return res;
+        s += to_string(root->val) + ",";
+        serializeHelper(root->left, s);
+        serializeHelper(root->right, s);
     }
-
-    TreeNode* deserialize(string data) {
-        if (data == "null") return nullptr;
-        stringstream ss(data);
-        string token;
-        getline(ss, token, ',');
-        TreeNode* root = new TreeNode(stoi(token));
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            TreeNode* node = q.front(); q.pop();
-            if (!getline(ss, token, ',')) break;
-            if (token != "null") {
-                node->left = new TreeNode(stoi(token));
-                q.push(node->left);
-            }
-            if (!getline(ss, token, ',')) break;
-            if (token != "null") {
-                node->right = new TreeNode(stoi(token));
-                q.push(node->right);
-            }
+    string serialize(TreeNode* root) {
+        string s;
+        serializeHelper(root, s);
+        return s;
+    }
+    TreeNode* deserializeHelper(vector<string>& nodes, int& index) {
+        if (nodes[index] == "#") {
+            index++;
+            return NULL;
         }
+        TreeNode* root = new TreeNode(stoi(nodes[index]));
+        index++;
+        root->left = deserializeHelper(nodes, index);
+        root->right = deserializeHelper(nodes, index);
         return root;
+    }
+    TreeNode* deserialize(string data) {
+        vector<string> nodes;
+        string temp;
+        stringstream ss(data);
+        while (getline(ss, temp, ',')) {
+            nodes.push_back(temp);
+        }
+        int index = 0;
+        return deserializeHelper(nodes, index);
     }
 };
