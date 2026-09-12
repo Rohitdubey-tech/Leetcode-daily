c8ddf579da2c0143db270139f0dd22bbc50806eb
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-13 00:37:18 +0530

    Time: 43 ms (34.14%) | Memory: 72.5 MB (62.42%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -4,7 +4,6 @@ public:
         priority_queue<int, vector<int>, greater<int>>pq;
         for(int num : nums){
             pq.push(num);
-        
         if(pq.size()>k){
             pq.pop();
         }
