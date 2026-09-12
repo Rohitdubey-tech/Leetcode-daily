a04cd16a349d9e342f776a698f4d9f9d4536d99a
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-13 01:00:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (76.04%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -5,19 +5,18 @@ public:
         for(int num : nums){
             mp[num]++;
         }
-        priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>>pq;
-        for(auto &[num, count]: mp){
-            pq.push({count, num});
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        for(auto it : mp){
+            pq.push({it.second, it.first});
             if(pq.size()>k){
                 pq.pop();
             }
         }
-        vector<int> ans;
+        vector<int>ans;
         while(!pq.empty()){
             ans.push_back(pq.top().second);
             pq.pop();
         }
         return ans;
-
     }
 };
