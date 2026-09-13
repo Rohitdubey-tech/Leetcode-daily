c90e97b708e1085351681a2ebeaf24c3e36550ee
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-13 18:42:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (53.50%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
+        int first = -1;
+        int last = -1;
+        int low = 0; 
+        int high = nums.size()-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid]==target){
+                first = mid;
+                high = mid-1;
+            }
+            else if(nums[mid] < target){
+                low = mid +1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        
+        low = 0;
+        high = nums.size()-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                last = mid;
+                low = mid+1;
+            }
+            else if(nums[mid] < target){
+                low = mid +1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
         return {first, last};
-    }
-    int findFirst(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1, ans = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                right = mid - 1; 
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return ans;
-    }
-    int findLast(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1, ans = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                left = mid + 1; 
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return ans;
-    }
+
+ }
 };
