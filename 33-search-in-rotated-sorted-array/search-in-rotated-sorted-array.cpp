382ebe64226496fcf9ad908fee29932ed679f7d1
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-13 19:23:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (37.38%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -10,16 +10,15 @@ public:
             }
             else if(nums[low] <= nums[mid]){
                 if(nums[low] <= target && target < nums[mid]){
-                    high = mid -1;
+                    high = mid-1;
                 }
                 else{
                     low = mid+1;
                 }
-
             }
-            else{
-                if(nums[mid]<target && target<=nums[high]){
-                    low = mid +1;
+            else {
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid+1;
                 }
                 else{
                     high = mid-1;
