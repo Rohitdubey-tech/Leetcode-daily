73f26859e4ac3b53d3849146ba6c4dce0288c33b
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-17 19:40:05 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (28.36%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -8,12 +8,9 @@ public:
         int leftSum = 0;
         for(int i=0; i<nums.size(); i++){
             int rightSum = totalSum - leftSum - nums[i];
-            if(leftSum == rightSum){
-                return i;
-            }
+            if(leftSum == rightSum) return i;
             leftSum += nums[i];
         }
         return -1;
     }
-
 };
