75f3cf80cf9e6c1b83e81cf7341c04aa480fc390
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-21 00:44:23 +0530

    Time: 3 ms (78.42%) | Memory: 53.1 MB (76.32%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>ans;
         for(int i=0; i<nums.size(); i++){
             int index = abs(nums[i])-1;
-            if(nums[index]>0){
+            if(nums[index] >0){
                 nums[index] = -nums[index];
             }
         }
