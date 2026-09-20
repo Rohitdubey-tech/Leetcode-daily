f8422aa6dce5791e88b8c56323f95314b3bc4861
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-20 20:16:01 +0530

    Time: 19 ms (81.14%) | Memory: 17.4 MB (87.43%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>result;
+        vector<vector<int>>ans;
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        int n=nums.size();
-        for(int i=0; i<n-3; i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1; j<n-2; j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                long long newTarget = (long long)target - nums[i] - nums[j];
-                int left = j + 1;
-                int right = n - 1;
-
-                 while (left < right) {
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            for(int j=i+1; j<n-1; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+                long long newTarget = (long long)target - nums[i]-nums[j];
+                int left = j+1;
+                int right = n-1;
+                while(left < right){
                     long long sum = nums[left] + nums[right];
-                    if (sum == newTarget) {
-                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
+                    if(sum == newTarget){
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while(left < right && nums[left]== nums[left+1]) left++;
+                        while(left < right && nums[right]==nums[right-1]) right--;
                         left++;
                         right--;
-                    } 
-                     else if (sum < newTarget) left++;
-                    else right--;
-                 }      
+                    }
+                    else if(sum < newTarget){
+                        left++;
+                    }
+                    else{
+                        right--;
+                    }
+                }
             }
         }
-        return result;
+        return ans;
     }
 };
