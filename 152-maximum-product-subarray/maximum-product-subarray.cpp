458be6385ee2e22d63918d9617e973d5f54390cf
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-20 20:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (23.13%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -6,8 +6,8 @@ public:
         int ans = nums[0];
         for(int i=1; i<nums.size(); i++){
             int temp = currMax;
-            currMax = max({nums[i], nums[i]*currMax, nums[i]*currMin});
-            currMin = min({nums[i], nums[i]*currMin, nums[i]*temp});
+            currMax = max({nums[i], nums[i]* currMax, nums[i]* currMin});
+            currMin = min({nums[i], nums[i]*currMin, nums[i]* temp});
             ans = max(ans, currMax);
         }
         return ans;
