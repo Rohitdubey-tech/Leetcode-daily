f561b1bc6fe9fc359d04a3142b379ad3ba63b1fe
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-22 01:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (58.86%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -11,8 +11,8 @@ public:
                 break;
             }
         }
-        for(int j=0; j<col; j++){
-            if(matrix[0][j]==0){
+        for(int i=0; i<col; i++){
+            if(matrix[0][i]==0){
                 firstRow = true;
                 break;
             }
@@ -27,16 +27,16 @@ public:
         }
         for(int i=1; i<row; i++){
             for(int j=1; j<col; j++){
-                if(matrix[0][j]==0 || matrix[i][0]==0){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
         if(firstRow){
-            for(int j=0; j<col; j++){
-                matrix[0][j]=0;
-            }        
+            for(int i=0; i<col; i++){
+                matrix[0][i]=0;
             }
+        }
         if(firstCol){
             for(int i=0; i<row; i++){
                 matrix[i][0]=0;
