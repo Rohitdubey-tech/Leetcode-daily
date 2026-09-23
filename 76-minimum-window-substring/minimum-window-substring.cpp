4a2a9bbca48058d4615228f8c7401a780966df2b
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2026-09-23 06:08:31 +0530

    Time: 10 ms (49.91%) | Memory: 11.5 MB (43.77%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int>mp;
+        unordered_map<char,int>mp;
         for(char c : t){
             mp[c]++;
         }
@@ -9,18 +9,18 @@ public:
         int start = 0;
         int minLength = INT_MAX;
         int required = t.size();
-        for(int right = 0; right <s.size(); right++){
-            if(mp[s[right]]> 0){
+        for(int right = 0; right<s.size(); right++){
+            if(mp[s[right]]>0){
                 required--;
             }
             mp[s[right]]--;
             while(required == 0){
-                if(right-left+1 < minLength){
+                if(right-left+1< minLength){
                     minLength = right-left+1;
                     start = left;
                 }
                 mp[s[left]]++;
-                if(mp[s[left]]> 0) required++;
+                if(mp[s[left]]>0) required++;
                 left++;
             }
         }
